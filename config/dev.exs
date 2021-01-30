b7962e5bac629b9b0c07f35bf96d5710634ec363
Adam Sebzda <asebzda@gmail.com>
2021-01-30 11:16:43 -0500

    Provide example env-specific config

--- config/dev.exs
+++
@@ -1,6 +0,0 @@
-import Config
-
-config :yams,
-  ca_cert: "./certs/TestAuth.crt",
-  server_cert: "./certs/TestServer.crt",
-  server_key: "./certs/TestServer.key"
---
+++ config/env-example.ex
@@ -0,0 +1,8 @@
+import Config
+
+# This is an example environment-specific configuration.
+
+config :yams,
+  ca_cert: "path/to/server_ca.crt",
+  server_cert: "path/to/server.crt",
+  server_key: "path/to/server.key"
