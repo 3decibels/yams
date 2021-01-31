ac51d0b0f47f5dac026078fc3663d26740ade912
Adam Sebzda <asebzda@gmail.com>
2021-01-30 23:50:55 -0500

    Convert serial index to primary key

--- priv/repo/migrations/20210130210845_create_cert_auth.exs
+++ priv/repo/migrations/20210130210845_create_cert_auth.exs
@@ -3,13 +3,12 @@ defmodule Yams.Database.Repo.Migrations.CreateCertAuth do
 
   def change do
     create table("cert_auth") do
-      add :serial, :integer, null: false
+      add :serial, :integer, null: false, primary_key: true
       add :common_name, :string, null: false
       add :encoded_cert, :string, null: false
       add :expiration, :utc_datetime, null: false
       add :active, :boolean, null: false
     end
-    create index("cert_auth", [:serial], unique: true)
     create index("cert_auth", [:common_name])
   end
 
