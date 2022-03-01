5af3156a730c668a8731e96d7e880e4c75cb776f
Adam Sebzda <asebzda@gmail.com>
2022-03-01 10:26:47 -0500

    Refactor authenticator

--- lib/server/connection/authenticator.ex
+++ lib/server/connection/authenticator.ex
@@ -18,35 +18,42 @@ defmodule Yams.Server.Connection.Authenticator do
   Returns `:ok` on success or `{:error, reason}` on failure.
   """
   def run(tls_socket, supervisor) do
-    # Authenticate connection and place under the passed dynamic supervisor.
-    # Use case to evaluate return from separate auth function and start child under
-    #   supervisor or simply exit.
     with {:ok, cert} <- :ssl.peercert(tls_socket),
-         %{serial_number: cert_serial, subject: cert_subject} <- EasySSL.parse_der(cert)
+         %{serial_number: cert_serial, subject: cert_subject} <- EasySSL.parse_der(cert),
+         :ok <- cert_is_active?(cert_serial)
     do
-      query = from c in CertAuth,
-        where: c.serial == ^cert_serial,
-        where: c.active == true
-      case Yams.Database.Repo.exists?(query) do
-        false -> 
-          :ssl.send(tls_socket, "Error: Authentication failed")
-          :ssl.close(tls_socket)
-          {:error, :auth_failed}
-        true -> 
-          #{:ok, pid} = DynamicSupervisor.start_child(supervisor, {Connection, %Connection{tls_socket: tls_socket}})
-          {:ok, _pid} = DynamicSupervisor.start_child(supervisor, {Yams.Server.Connection.Echo,
-            %Connection{tls_socket: tls_socket, client_name: cert_subject[:CN], distinguished_name: cert_subject[:aggregated]}})
-          :ok
-      end
+      {:ok, _pid} = DynamicSupervisor.start_child(supervisor, {Yams.Server.Connection.Echo,
+        %Connection{tls_socket: tls_socket, client_name: cert_subject[:CN], distinguished_name: cert_subject[:aggregated]}})
+      :ok
     else
+      {:error, :auth_error} ->
+        :ssl.send(tls_socket, "Error: Authentication failed\n")
+        :ssl.close(tls_socket)
+        {:error, :auth_failed}
       {:error, _reason} ->
-        :ssl.send(tls_socket, "Error: Could not parse client cert")
+        :ssl.send(tls_socket, "Error: Could not parse client cert\n")
         :ssl.close(tls_socket)
         {:error, :bad_cert}
       _ ->
-        :ssl.send(tls_socket, "Error: Could not authenticate client")
+        :ssl.send(tls_socket, "Error: Could not authenticate client\n")
         :ssl.close(tls_socket)
         {:error, :unknown}
+    end
+  end
+
+
+  @doc """
+  Checks a certificate serial number against the cert database to see if it exists and is active.
+
+  Returns `:ok` on success or `{:error, reason}` on failure.
+  """
+  def cert_is_active?(serial) when is_binary(serial) do
+    query = from c in CertAuth,
+        where: c.serial == ^serial,
+        where: c.active == true
+    case Yams.Database.Repo.exists?(query) do
+        false -> {:error, :auth_error}
+        true  -> :ok
     end
   end
 
